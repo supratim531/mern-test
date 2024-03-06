d2b77424d8c9120415c83eccabe6ac6659d9394a
supratim531 <91423964+supratim531@users.noreply.github.com>
2024-03-06 15:49:59 +0530

    supratim531: Added merntestfrontend.com to the cors-origin list

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = 8000 || process.env.PORT;
 
 app.use(cors({
-  origin: "http://localhost:3000"
+  origin: ["http://localhost:3000", "https://merntestfrontend.vercel.app"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
